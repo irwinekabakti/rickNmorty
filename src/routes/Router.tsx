e1e28c2256445de9cf91620ea71bf2d06f05ca49
irwinekabakti <irwinekabakti@gmail.com>
2023-06-24 20:40:47 +0700

    add:(setup router)

---
+++ src/routes/Router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "./RootLayout/RootLayout";
+import Homepage from "../pages/Homepage/Homepage";
+import Detailpage from "../pages/Detailpage/Detailpage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    // errorElement,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "detail/:id",
+        element: <Detailpage />,
+      },
+    ],
+  },
+]);
+
+export default router;
